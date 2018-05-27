3b15a6662f9cf3a602c94a8b53ddd9891ab493ca
Victor Cueva <Ingvcueva@gmail.com>
2018-05-27 03:52:24 -0500

    Agregando data temporal en movimiento

--- routes/movimiento.js
+++ routes/movimiento.js
@@ -2,12 +2,23 @@
 const router = require('express').Router();
 const createError = require('http-errors');
 
+let data = [
+	{ "id": 1, "fecha": "01/10/2018", "monto": 823.12, "categoria": "ingreso", "descripcion": "Freelance de Ecommerce" },
+	{ "id": 2, "fecha": "02/10/2018", "monto": 157.23, "categoria": "gasto", "descripcion": "Dia de la madre" },
+	{ "id": 3, "fecha": "03/10/2018", "monto": 87.21, "categoria": "ingreso", "descripcion": "Consultoría" },
+	{ "id": 4, "fecha": "04/10/2018", "monto": 120.21, "categoria": "gasto", "descripcion": "Uber" },
+	{ "id": 5, "fecha": "05/10/2018", "monto": 56.12, "categoria": "gasto", "descripcion": "Salida romantica" },
+	{ "id": 6, "fecha": "06/10/2018", "monto": 789.21, "categoria": "ingreso", "descripcion": "Freelance de Desarrollo de sitio web" }
+];
+
 router.get('/', (req, res, next) => {
-	next(createError(405));
+	res.json(data);
 });
 
 router.get('/:id', (req, res, next) => {
-	next(createError(405));
+	let id = req.params.id;
+	
+	res.json(data[0]);
 });
 
 router.post('/', (req, res, next) => {
